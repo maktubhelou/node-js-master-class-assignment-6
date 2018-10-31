5278c639688db8536b898bc889ad1a1b1ded732d
Mark Evans <markallanevans@gmail.com>
2018-10-31 08:57:34 +0700

    refactored for multiple cores

---
+++ README.md
@@ -0,0 +1,7 @@
+# Node.js Master Class - Assignment #1
+
+## Hello World
+
+- This is a RESTful JSON API homework assignment.
+- The API accepts a localhost URL with a pathname. If the pathname "hello" is requested it will return a JSON formatted greeting.
+- If an unknown path is requested it will return a 404 status code with a message.
--- index.js
+++ index.js
@@ -1,112 +1,17 @@
-/*
-* Primary file for the API
-*
-*/
+const server = require("./lib/server");
+const cluster = require("cluster");
+const os = require("os");
 
-// ENV
-const config = require('./lib/config');
-const httpPORT = config.httpPort;
-const httpsPORT = config.httpsPort;
+const app = {};
 
-// Dependencies
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const fs = require('fs');
-const StringDecoder = require('string_decoder').StringDecoder;
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
-
-// Instantiate the HTTP server
-const httpServer = http.createServer(function(req, res) {
-    unifiedServer(req, res);
-});
-
-// Start the server
-httpServer.listen(httpPORT, function(){
-    console.log(`The server is listening on port ${httpPORT}.`);
-});
-
-// Instantiate the HTTPS server
-const httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem')
-};
-const httpsServer = https.createServer(httpsServerOptions, function(req, res) {
-    unifiedServer(req, res);
-});
-
-// Start the HTTPS server
-httpsServer.listen(httpsPORT, function(){
-    console.log(`The server is listening on port ${httpsPORT}.`);
-});
-
-
-
-// All the server logic for both the http and https servers
-const unifiedServer = function(req, res) {
-
-    // Get the URL and parse it
-    const parsedUrl = url.parse(req.url, true);
-
-    // Get the path from the URL
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    // Get the query string as an object
-    const queryStringObject = parsedUrl.query;
-
-    // Get the HTTP Method
-    const method = req.method.toLowerCase();
-
-    // Get the headers as an object
-    const headers = req.headers;
-
-    // Get the payload
-    const decoder = new StringDecoder('utf-8');
-    let buffer = '';
-    req.on('data', function(data){
-        buffer += decoder.write(data);
-    });
-    req.on('end', function(){
-        buffer += decoder.end();
-
-        // Chose the handler this request should go to.
-        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        
-        // construct data object to send to handler
-        const data = {
-            'trimmedPath': trimmedPath,
-            'queryStringObject': queryStringObject,
-            'method': method,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer)
-        }
-
-        // Route the request to the handeler specified in the router
-        chosenHandler(data, function(statusCode, payload) {
-            // Use the statuse code called back by the handler, or default to 200
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-            // Use the payload called back by the handler or default to an empty object
-            payload = typeof(payload) == 'object' ? payload : {};
-
-            // Convert the payload to a string
-            const payloadString = JSON.stringify(payload);
-
-            // Return the response
-            res.setHeader('Content-type', 'application/json'); //tells the browser the type of content
-            res.writeHead(statusCode);
-            res.end(payloadString);
-            console.log(`Returning this response:`, statusCode, payloadString);
-        });
-    });
+app.init = () => {
+  if (cluster.isMaster) {
+    for (i = 0; i < os.cpus().length; i++) {
+      cluster.fork();
+    }
+  } else {
+    server.init();
+  }
 };
 
-// Define a request router
-const router = {
-    'ping': handlers.ping,
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks
-};
+app.init();
